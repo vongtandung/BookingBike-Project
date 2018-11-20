53fd84592b800db82c0f26675f8c555474c4336c
Vòng Tần Dũng <32818357+vongtandung@users.noreply.github.com>
2018-11-20 21:13:05 +0700

    add controller update api

--- Back-End/apiControllers/driverController.js
+++ Back-End/apiControllers/driverController.js
@@ -2,12 +2,23 @@ var express = require('express'),
     moment = require('moment'),
     low = require('lowdb'),
     fileSync = require('lowdb/adapters/FileSync'),
-    requestRepo = require('../repos/requestRepo');
+    requestRepo = require('../repos/requestRepo'),
+    driverRepo = require('../repos/driverRepo');
 
     var router= express.Router();
     router.post('/finish',(req,res)=> {
-        console.log(req)
         requestRepo.updateState('Finished', req.body.requestid).then( 
+            res.json(
+                {"mess" : "OK"}
+            )
+        )
+        .catch(err=>{
+            console.log(err);
+            res.end(err);
+        })
+    });
+    router.post('/update',(req,res)=> {
+        driverRepo.updateLocate(req.body).then( 
             res.json(
                 {"mess" : "OK"}
             )
