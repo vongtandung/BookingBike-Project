49405407d75d622727a63695533016d97f30f68f
Vòng Tần Dũng <32818357+vongtandung@users.noreply.github.com>
2018-11-21 11:12:14 +0700

    update server

--- socket.js
+++ socket.js
@@ -1,53 +1,114 @@
 var http = require("http").createServer();
 var socketPort = process.env.PORT || 3002;
 server = require("socket.io")(socketPort);
+requestRepo = require("../Back-End/repos/requestRepo");
 var io = server;
 var arrayDriver = [];
 var arrayLocaIder = [];
 var arrayManager = [];
-
+const numberDriver = 5;
 io.on("connection", function(socket) {
-  console.log("user connect" + socket.id);
-  socket.on("disconnect", function() {
-    if (arrayDriver.length > 0) {
-      if (arrayDriver.includes(soket.id)) {
-        arrayDriver.pop(soket.id);
-      }
-    }
-    if (arrayLocaIder.length > 0) {
-      if (arrayLocaIder.includes(soket.id)) {
-        arrayLocaIder.pop(soket.id);
-      }
-    }
-    if (arrayManager.length > 0) {
-      if (arrayManager.includes(soket.id)) {
-        arrayManager.pop(soket.id);
-      }
-    }
-  });
-  socket.on("user-send-place", function(place) {
-    if (arrayLocaIder.length > 0) {
-      forEach(arrayLocaIder, function(element) {
-        io.to(element).emit("server-send-place2", place);
-      });
+  console.log(socket.id);
+  console.log("name: " + socket.handshake.query.name);
+  person = {
+    socketid: socket.id,
+    name: socket.handshake.query.name,
+    id: socket.handshake.query.id,
+    permission: socket.handshake.query.permission
+  };
+  Listarr = [];
+  if (socket.handshake.query.permission === "1") {
+    console.log("User " + person.name + " Is connect");
+  } else {
+    if (socket.handshake.query.permission === "2") {
+      console.log("Location Identifier " + person.name + " Is connect");
+      arrayLocaIder.push(person);
     } else {
-      console.log("Error");
+      if (socket.handshake.query.permission === "3") {
+        console.log("Manager " + socket.handshake.query.name + " Is connect");
+        arrayManager.push(person);
+      } else {
+        if (person.permission === "4") {
+          console.log("Driver " + person.name + " Is connect");
+          arrayDriver.push(person);
+        }
+      }
+    }
+  }
+  socket.on("driver-send-response", function(resp) {
+    if (resp.mess === "accept") {
+     
+        io.to(resp.socketid).emit(
+          "server-send-response-user",
+          requestid
+        );
+    }
+    if (resp.mess === "reject") {
+      if (Listarr.length > 1) {
+        const dri = Listarr.filter((per, index) => {
+          return per.socketid === socket.id;
+        });
+        Listarr.splice(Listarr.indexOf(dri[0]), 1);
+        io.to(Listarr[0].socketid).emit(
+          "server-send-request-driver",
+          resp.requestid
+        )
+      }
+        else {
+          
+        };
+      }
+  });
+  socket.on("user-send-place", function(data) {
+    if (arrayLocaIder.length > 0) {
+      const index = Math.floor(Math.random() * arrayLocaIder.length + 0);
+      const request = {
+        idUser: data.id,
+        beginPlace: data.place,
+        time: Date.now()
+      };
+      console.log("array kocate leng: " + arrayLocaIder.length);
+      requestRepo.addRequest(request).then(
+        requestRepo.getRequestId(data.phone).then(row => {
+          io.to(arrayLocaIder[index].socketid).emit(
+            "server-send-place-locate",
+            row[0].id
+          );
+        })
+      );
+    } else {
+      console.log("No location Identifier is on");
     }
   });
-  socket.on("driver-login", function() {
-    if (!arrayDriver.indexof(soket.id)) {
-      arrayDriver.push(soket.id);
-    }
+  socket.on("locate-send-result", function(requestid) { 
+    ele = arrayDriver.filter((per, index) => {
+      return per.id === socket.handshake.query.id;
+    });
+    io.to(ele[0].socketid).emit("server-send-request-driver", requestid);
   });
-  socket.on("location-identifier-login", function() {
-    if (!arrayLocaIder.indexof(soket.id)) {
-      arrayDriver.push(soket.id);
+
+  socket.on("disconnect", function() {
+    if (person.permission === "2") {
+       ele = arrayLocaIder.filter((per, index) => {
+        return per.socketid === socket.id;
+      });
+      arrayLocaIder.splice(arrayLocaIder.indexOf(ele[0]), 1);
+    } else {
+      if (person.permission === "3") {
+         ele = arrayManager.filter((per, index) => {
+          return per.socketid === socket.id;
+        });
+        arrayManager.splice(arrayManager.indexOf(ele[0]), 1);
+      } else {
+        if (person.permission === "4") {
+          ele = arrayDriver.filter((per, index) => {
+            return per.socketid === socket.id;
+          });
+          arrayDriver.splice(arrayDriver.indexOf(ele[0]), 1);
+        }
+      }
     }
-  });
-  socket.on("Manager-login", function() {
-    if (!arrayDriver.indexof(soket.id)) {
-      arrayManager.push(soket.id);
-    }
+    console.log(person.socketid + "is disconect");
   });
 });
 
@@ -56,3 +117,4 @@ console.log(`WS running on port ${socketPort}`);
 module.exports = {
   io
 };
+
