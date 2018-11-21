42172283570592d2a5587e60cb668330101088f9
Vòng Tần Dũng <32818357+vongtandung@users.noreply.github.com>
2018-11-21 11:48:38 +0700

    update

--- Back-End/apiControllers/locateController.js
+++ Back-End/apiControllers/locateController.js
@@ -35,56 +35,7 @@ router.post("/located", (req, res) => {
       res.end(err);
     });
 });
-router.post("/driverlist", (req, res) => {
-  driverRepo
-    .getAllDriverLocate()
-    .then(row => {
-      if (row.length > 0) {
-        for (i = 0; i < row.length; i++) {
-          row[i].distance = distanceWithHaversin(
-            req.body.lat,
-            req.body.lng,
-            row[i].lat,
-            row[i].lng
-          );
-        }
-        row.sort(function(a,b){
-            var x =  a.distance;
-            var y = b.distance;
-            if(x<y) {return -1}
-            else{return 1};
-        });
-        res.json(row);
-      }
-      else{
-          res.end("No driver is found");
-      }
-    })
-    .catch(err => {
-      console.log("err when find driver");
-      res.end(err);
-    });
-});
 
 module.exports = router;
 
-function distanceWithHaversin(lat1, lon1, lat2, lon2) {
-  var R = 6371e3; // metres
-  var p1 = toRadians(lat1);
-  var p2 = toRadians(lat2);
-  var dentalat = toRadians(lat2 - lat1);
-  var dentalon = toRadians(lon2 - lon1);
 
-  var a =
-    Math.sin(dentalat / 2) * Math.sin(dentalat / 2) +
-    Math.cos(p1) *
-      Math.cos(p2) *
-      Math.sin(dentalon / 2) *
-      Math.sin(dentalon / 2);
-  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-
-  return R * c;
-}
-function toRadians(Value) {
-  return (Value * Math.PI) / 180;
-}
