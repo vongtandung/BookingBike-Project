3898d5cb8c1ac27ec163f252de23a4078085a0f0
Vòng Tần Dũng <32818357+vongtandung@users.noreply.github.com>
2018-11-21 11:11:44 +0700

    update repo

--- Back-End/repos/driverRepo.js
+++ Back-End/repos/driverRepo.js
@@ -18,3 +18,7 @@ exports.setFree =(driverid) =>{
     var sql = `update driverlocate set st=1 where driverid = '${driverid}'`;
     return db.insert(sql);
 }
+exports.getAllDriverLocate=()=>{
+    var sql = `select * from driverlocate where st=1`;
+    return db.load(sql);
+}
