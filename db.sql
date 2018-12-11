8075d45d0047ef10f91413c20e7613b936de5cd5
Vòng Tần Dũng <32818357+vongtandung@users.noreply.github.com>
2018-12-11 13:28:18 +0700

    add db

--- db.sql
+++ db.sql
@@ -37,7 +37,7 @@ CREATE TABLE `driverlocate` (
 
 LOCK TABLES `driverlocate` WRITE;
 /*!40000 ALTER TABLE `driverlocate` DISABLE KEYS */;
-INSERT INTO `driverlocate` VALUES (4,12.345,12.2,0),(8,2,2,1);
+INSERT INTO `driverlocate` VALUES (4,10.7661,106.664,2),(7,10.7661,106.664,2),(8,10.7681,106.663,0),(9,10.7622,106.642,0);
 /*!40000 ALTER TABLE `driverlocate` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -50,16 +50,17 @@ DROP TABLE IF EXISTS `request`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `request` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `idUser` int(11) DEFAULT NULL,
   `idDriver` int(11) DEFAULT NULL,
   `BeginPlace` varchar(300) CHARACTER SET utf8 DEFAULT NULL,
   `lat` float DEFAULT NULL,
   `lng` float DEFAULT NULL,
   `Note` varchar(200) CHARACTER SET utf8 DEFAULT NULL,
-  `Time` varchar(20) DEFAULT NULL,
+  `Time` bigint(20) DEFAULT NULL,
   `State` tinytext,
+  `CusName` tinytext NOT NULL,
+  `CusPhone` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=242 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=552 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -68,7 +69,7 @@ CREATE TABLE `request` (
 
 LOCK TABLES `request` WRITE;
 /*!40000 ALTER TABLE `request` DISABLE KEYS */;
-INSERT INTO `request` VALUES (237,1,NULL,'Avenida Rivadavia 5160, Zarate, Buenos Aires, Argentina',NULL,NULL,NULL,'1542546892638','Located'),(238,1,NULL,'Avenida Rivadavia 5160, Zarate, Buenos Aires, Argentina',NULL,NULL,NULL,'1542546921108','requesting'),(239,1,NULL,'Avenida Rivadavia 5160, Zarate, Buenos Aires, Argentina',NULL,NULL,NULL,'1542546947318','Located'),(240,1,4,'Avenida Rivadavia 5160, Zarate, Buenos Aires, Argentina',12.345,12.2,NULL,'1542546973255','Waiting'),(241,1,7,'Avenida Rivadavia 5160, Zarate, Buenos Aires, Argentina',NULL,NULL,NULL,'1542546989166','waiting');
+INSERT INTO `request` VALUES (538,4,'49 Xa lộ Hà Nội, Thảo Điền, District 2, Ho Chi Minh City, Vietnam',10.8009,106.734,'hehe',1544345882264,'Finished','Duy',1258906910),(539,4,'49 Xa lộ Hà Nội, Thảo Điền, District 2, Ho Chi Minh City, Vietnam',10.7993,106.721,'asdasd',1544346043659,'Finished','duy',12412323),(540,NULL,'46 Phan Xích Long, phường 3, Phú Nhuận, Ho Chi Minh City, Vietnam',10.8031,106.682,'asdasd',1544346211896,'Cancel','dung',1231232),(541,NULL,'45 Phan Xích Long, phường 3, Phú Nhuận, Ho Chi Minh City, Vietnam',10.8032,106.682,'12123',1544346291305,'Cancel','dung',123123213),(542,4,'10 Xa lộ Hà Nội, Thảo Điền, District 2, Ho Chi Minh City, Vietnam',10.8026,106.746,'dddd',1544347034058,'Finished','ha',12312333),(543,NULL,'35 Nguyễn Xí, phường 26, Bình Thạnh, Ho Chi Minh City, Vietnam',10.8111,106.711,'dasd',1544347125841,'Cancel','nam',12312312),(544,NULL,'49 Xa lộ Hà Nội, Thảo Điền, District 2, Ho Chi Minh City, Vietnam',10.8009,106.734,'asdasd',1544347616256,'located','ddasd',123123),(545,NULL,'19 Xa lộ Hà Nội, Thảo Điền, District 2, Ho Chi Minh City, Vietnam',10.8,106.73,'ddd',1544347626702,'Cancel','dddd',123123),(546,NULL,'49 Xa lộ Hà Nội, Thảo Điền, District 2, Ho Chi Minh City, Vietnam',10.8009,106.734,'d',1544351603294,'located','asdasd',1312312),(547,NULL,'Santa Fe 4300, Rosario, Santa Fe Province, Argentina',-32.9372,-60.6818,'asdasd',1544351810724,'located','sdasd',123123),(548,NULL,'49 Xa lộ Hà Nội, Thảo Điền, District 2, Ho Chi Minh City, Vietnam',10.8009,106.734,'d',1544351980584,'located','asdasd',123123123),(549,NULL,'Avenida Rivadavia 1238, Buenos Aires, Argentina',-34.6087,-58.384,'d',1544352197326,'located','asdasd',123123),(550,NULL,'Avenida Rivadavia 1238, Buenos Aires, Argentina',-34.6087,-58.384,'ddd',1544352280065,'located','asd',123123213),(551,NULL,'Avenida Rivadavia 1238, Buenos Aires, Argentina',NULL,NULL,'1111',1544352296164,'requesting','1',1111);
 /*!40000 ALTER TABLE `request` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -88,7 +89,7 @@ CREATE TABLE `user` (
   `Permission` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -97,7 +98,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES ('Ta','4QrcOUm6Wau+VuBX8g+IPg==',1,'Hoai Minh',125678392,1),('too','4QrcOUm6Wau+VuBX8g+IPg==',2,'chi ',178928321,2),('Jun','4QrcOUm6Wau+VuBX8g+IPg==',3,'Junlila',167894612,3),('wendy','4QrcOUm6Wau+VuBX8g+IPg==',4,'John',125678932,4),('duy','4QrcOUm6Wau+VuBX8g+IPg==',5,'duy',1223344,1),('a','4QrcOUm6Wau+VuBX8g+IPg==',6,'Minh',122222,2),('b','4QrcOUm6Wau+VuBX8g+IPg==',7,'Le',1111,4),('c','4QrcOUm6Wau+VuBX8g+IPg==',8,'mina',11223,4);
+INSERT INTO `user` VALUES ('Ta','4QrcOUm6Wau+VuBX8g+IPg==',1,'Hoai Minh',125678392,1),('too','4QrcOUm6Wau+VuBX8g+IPg==',2,'chi ',178928321,2),('Jun','4QrcOUm6Wau+VuBX8g+IPg==',3,'Junlila',167894612,3),('wendy','4QrcOUm6Wau+VuBX8g+IPg==',4,'John',125678932,4),('duy','4QrcOUm6Wau+VuBX8g+IPg==',5,'duy',1223344,1),('a','4QrcOUm6Wau+VuBX8g+IPg==',6,'Minh',122222,2),('b','4QrcOUm6Wau+VuBX8g+IPg==',7,'Le',1111,4),('c','4QrcOUm6Wau+VuBX8g+IPg==',8,'mina',11223,4),('d','4QrcOUm6Wau+VuBX8g+IPg==',9,'Jespine',11123412,4);
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -122,7 +123,7 @@ CREATE TABLE `userRefreshTokenExt` (
 
 LOCK TABLES `userRefreshTokenExt` WRITE;
 /*!40000 ALTER TABLE `userRefreshTokenExt` DISABLE KEYS */;
-INSERT INTO `userRefreshTokenExt` VALUES (1,'MUVGFKSLGf8tIhApKEfmaniJnvheIy543xuzNs00lfqaREQfFME7NOMu2Os2wZRKSckEqb79YChv7ApW','2018-11-18 15:32:26'),(2,'mCWxX2l32ab4QeulQf8DkZCzen0yGr9UoIyTURold131cYDlMKlHuyGMgAZidTM3Tpk2wg2mFaGKobB5','2018-11-18 14:58:08'),(3,'vk3Lr5Blh7M9cBu5ky1i2Tgcgw7lKv2IMkbaMFTEDTt9UV8jleJ2wi19pDDeozV8OxsQMpC0ohe20Y6H','2018-11-15 15:08:36'),(4,'yi4oI83WY8B2qdzeI5Vn8HKZIF1VSdTMchfero5zdAWzre2z1tXKsYLBLgo8onON0SlORaub1oznd4xZ','2018-11-18 19:10:39'),(5,'tLQ0o3eQ5WiBEa1XwAMLClxqM0nVUZ6JiuegGYJCSfuW3qQYJoB3VKTLr8VbThOc1C1lPjDXUwBNaJAw','2018-11-18 15:51:33'),(6,'TDjtCCz5KYKtJ9tTpW20ixS4yf8OknO4mzO3VDTQbxVrcwgqV0zVp7vpz7itbncYbvGFB6wnH3EC2qVI','2018-11-18 15:51:50'),(7,'64U2uDJTaJAhbjfWh2krjNiuvZZWLA43xoGIQ4NykP7bQCNPllMV71GefKN3P9Ezo5j8PfjGfZcFhFXv','2018-11-18 19:16:20'),(8,'ysoJklrCV8Vpcalo9c9hzmLOp5a6lwUiYo9kUtqhQoi8wRb96V8kriYXHIv8V5xSnYemHWpKHY4tSkVO','2018-11-18 19:15:44');
+INSERT INTO `userRefreshTokenExt` VALUES (1,'4DUWaDCb1TVu2t6t5REdPmzmwS2QRyCkyHncFDNkqHzIxmZUhIiCHSlLdYsTzlgT6vrbDgFGjl5me2aK','2018-12-09 17:44:21'),(2,'v63LJ9mj7L09Ef1dgxQZ3NQJHpZYC5t6pbn41UV8CUz1n847HVgx55Urkf1QG6UeWfbaXoPWh0klY98D','2018-11-24 18:30:29'),(3,'QFDJHmDxwjOaemwJHsUaDv2DXyqfi5VYGzNjRnCJK9w7yact1ICRCHqGugvFyvHbWIIvAMrxozhgFZwR','2018-12-09 16:09:09'),(4,'L645Q8ZZOglCAdtOKGiTnnuNlzr3CJABm1CZMOKqjuh983KTrna4S14V6QdzZuvAFTKa7uyn6kH6GVVI','2018-12-09 16:26:17'),(5,'ErdRzhmlbxQv3d8shoY7wvTVQ0EPvqfUd0TzPi9pIby91hEJLl1WmLhEyBvsNDx2eAULdBVp1rySOsEf','2018-12-09 16:26:28'),(6,'NV65igKKvY2tf90P9zKLIOLhON3DjEy4vBK3E1EkDbn8NzNmGRFTHtICf4RCCCS3BJ9WNoYa9eNTj0EQ','2018-12-09 17:33:10'),(7,'jkGHlQ6s42iej8jeN4AJGAwpUD7ktlBipsvCoEvidFrmrIqnZdVtzscXDTfdiLL4dVMa9bWsLzfcXaCd','2018-12-09 16:26:41'),(8,'aP4bALY2b7LOFmucryVitwCkZxdAG9y5cS3WNw7qtnZJbPTSznGZfxl7A73Sr0d7V7dHqTL8teqESAlx','2018-11-25 16:36:07'),(9,'tQVwD9mxZOf8zKeTfsU0HL0QwLvKtXewJ5uMfbD1xLMFJUXvjeF253ogumcrIlOdTDDyoIHKIibQZkxA','2018-11-24 21:23:04');
 /*!40000 ALTER TABLE `userRefreshTokenExt` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -135,4 +136,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-11-21  9:54:25
+-- Dump completed on 2018-12-11 13:27:23
